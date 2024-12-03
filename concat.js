af59b742b07a1b6db6f9bcf564ab4f0d871d231c
Tom Rudge <me@tomrudge.com>
2024-12-03 19:30:37 +0000

    Added concat & localise packages

--- concat.js
+++ concat.js
@@ -1,4 +1,3 @@
 var concat = require('concat');
-concat(['./dist/runtime.js',
-        './dist/polyfills.js',
-        './dist/main.js'],'./dist/my-app.js');
+concat(['./dist/demo/browser/en-US/polyfills.js',
+        './dist/demo/browser/en-US/main.js'],'./dist/my-app.js');
