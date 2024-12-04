46c0016d157819bc6749ce9465595086781466c9
Tom Rudge <me@tomrudge.com>
2024-12-04 08:51:35 +0000

    Update to dist files

--- src/concat.js
+++ src/concat.js
@@ -1,5 +1,5 @@
 var concat = require('concat');
 concat(
-  ['./dist/runtime.js', './dist/polyfills.js', './dist/main.js'],
+  ['./dist/demo/browser/polyfills.js', './dist/demo/browser/main.js'],
   './dist/my-app.js'
 );
