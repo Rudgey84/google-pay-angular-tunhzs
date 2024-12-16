25b3de870413afa0dd7b6eff163f219f86ef1f75
Tom Rudge <me@tomrudge.com>
2024-12-16 17:10:12 +0000

    Refactor IframeComponent for improved readability and structure

--- src/app/iframe.component.ts
+++ src/app/iframe.component.ts
@@ -1,107 +1,103 @@
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import {
-    AfterViewInit,
-    Component,
-    ComponentFactoryResolver,
-    ComponentRef,
-    ElementRef,
-    OnInit,
-    ViewChild,
-    ViewEncapsulation,
-    Inject,
-    ViewContainerRef
-  } from "@angular/core";
-  import { InnerComponent } from "./inner.component";
-  import { CommonModule } from '@angular/common';
+  AfterViewInit,
+  Component,
+  ComponentRef,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+  Inject,
+  ViewContainerRef
+} from "@angular/core";
+import { InnerComponent } from "./inner.component";
+import { CommonModule } from '@angular/common';
 
-  @Component({
-    selector: "hello-world",
- //   encapsulation: ViewEncapsulation.ShadowDom,
-    standalone: true,
-    imports: [CommonModule],
-    template: `
+@Component({
+  selector: "hello-world",
+  //   encapsulation: ViewEncapsulation.ShadowDom,
+  standalone: true,
+  imports: [CommonModule],
+  template: `
 <div *ngIf="isLoading" class="loading-container">
     <div class="loading"></div>
 </div>
 
     <iframe sandbox="allow-scripts allow-same-origin" #contentFrame>Loading...</iframe>
     `
-  })
-  export class IframeComponent implements OnInit, AfterViewInit {
-    private contentRef: ComponentRef<any> | null = null;
-    private frameDoc: Document | null = null;
-  
-    @ViewChild("contentFrame") contentFrame!: ElementRef;
+})
+export class IframeComponent implements OnInit, AfterViewInit {
+  private contentRef: ComponentRef<any> | null = null;
+  private frameDoc: Document | null = null;
 
-  
-    constructor(
-      private _mdr: MatDialogRef<IframeComponent>,
-      private resolver: ComponentFactoryResolver,
-      private viewContainerRef: ViewContainerRef,
-      @Inject(MAT_DIALOG_DATA) public data: {
-        requestKey: string;
-        sessionToken: string;
-        password: string;
-        baseUrl: string;
-        merchantId: string;
-      }
-    ) {
-      console.log("Received Data:", data);
+  @ViewChild("contentFrame") contentFrame!: ElementRef;
+
+
+  constructor(
+    private _mdr: MatDialogRef<IframeComponent>,
+    private viewContainerRef: ViewContainerRef,
+    @Inject(MAT_DIALOG_DATA) public data: {
+      requestKey: string;
+      sessionToken: string;
+      password: string;
+      baseUrl: string;
+      merchantId: string;
     }
+  ) {
+    console.log("Received Data:", data);
+  }
 
-    isLoading: boolean = true;
+  isLoading: boolean = true;
 
-    ngOnInit(): void {
-      const factory = this.resolver.resolveComponentFactory(InnerComponent);
-      this.contentRef = this.viewContainerRef.createComponent(factory);
-  
-      if (this.contentRef.instance) {
-        const instance = this.contentRef.instance;
-        instance.requestKey = this.data.requestKey;
-        instance.sessionToken = this.data.sessionToken;
-        instance.baseUrl = this.data.baseUrl;
-        instance.merchantId = this.data.merchantId;
-  
-        // Listen for the closeDialog event
-        instance.closeDialog.subscribe(() => {
-          this.CloseDialog();
-        });
+  ngOnInit(): void {
+    this.contentRef = this.viewContainerRef.createComponent(InnerComponent);
 
-        instance.loadingState.subscribe((loading: boolean) => {
-          this.isLoading = loading;
-        });
-      }
-    }
-  
-    public ngAfterViewInit(): void {
-      setTimeout(() => {
-        // Get reference to loaded iframe document
-        this.frameDoc =
-          this.contentFrame.nativeElement.contentDocument ||
-          this.contentFrame.nativeElement.contentWindow;
-          if (this.frameDoc && this.frameDoc.body) {
-            // Inject the component into the iframe
-            this.frameDoc.body.appendChild(this.contentRef?.location.nativeElement);
-    
-            // Inject global styles into the iframe
-            this.injectGlobalStyles();
-          }
-      }, 1000);
-    }
+    if (this.contentRef.instance) {
+      const instance = this.contentRef.instance;
+      instance.requestKey = this.data.requestKey;
+      instance.sessionToken = this.data.sessionToken;
+      instance.baseUrl = this.data.baseUrl;
+      instance.merchantId = this.data.merchantId;
 
-    injectGlobalStyles(): void {
-      if (this.frameDoc) {
-        const globalStyles = document.querySelectorAll('style, link[rel="stylesheet"]');
-        globalStyles.forEach((styleElement) => {
-          // Important these are cloned for security so they can't be updated
-          const clonedStyle = styleElement.cloneNode(true);
-          this.frameDoc?.head.appendChild(clonedStyle);
-        });
-      }
-    }
-  
-    CloseDialog() {
-      this._mdr.close(false);
+      // Listen for the closeDialog event
+      instance.closeDialog.subscribe(() => {
+        this.CloseDialog();
+      });
+
+      instance.loadingState.subscribe((loading: boolean) => {
+        this.isLoading = loading;
+      });
     }
   }
-  
+
+  public ngAfterViewInit(): void {
+    setTimeout(() => {
+      // Get reference to loaded iframe document
+      this.frameDoc =
+        this.contentFrame.nativeElement.contentDocument ||
+        this.contentFrame.nativeElement.contentWindow;
+      if (this.frameDoc && this.frameDoc.body) {
+        // Inject the component into the iframe
+        this.frameDoc.body.appendChild(this.contentRef?.location.nativeElement);
+
+        // Inject global styles into the iframe
+        this.injectGlobalStyles();
+      }
+    }, 1000);
+  }
+
+  injectGlobalStyles(): void {
+    if (this.frameDoc) {
+      const globalStyles = document.querySelectorAll('style, link[rel="stylesheet"]');
+      globalStyles.forEach((styleElement) => {
+        // Important these are cloned for security so they can't be updated
+        const clonedStyle = styleElement.cloneNode(true);
+        this.frameDoc?.head.appendChild(clonedStyle);
+      });
+    }
+  }
+
+  CloseDialog() {
+    this._mdr.close(false);
+  }
+}
